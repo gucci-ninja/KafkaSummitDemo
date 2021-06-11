b2f0e1c88158c7d571bd80f198adf916dadb3faf
gucci-ninja <beautyspell5@gmail.com>
2021-06-10 23:47:00 -0400

    🚗 Moved contract related logic to separate file

--- patentrail-be/server/index.js
+++ patentrail-be/server/index.js
@@ -1,49 +1,17 @@
 const patents = require('./patents.js');
 const express = require("express");
-const PatentStore = require("../build/contracts/Patents.json");
-
-var Web3 = require("web3");
 
 const PORT = process.env.PORT || 8000;
 
 const app = express();
 app.use(express.json())
 
-const web3 = new Web3('ws://localhost:7545');
-var PatentContract;
-
-initializeContract()
-
-async function initializeContract() { 
-  const networkId = await web3.eth.net.getId()
-  const deployedAddress = PatentStore.networks[networkId].address
-  console.log(deployedAddress);
-  PatentContract = new web3.eth.Contract(PatentStore.abi, deployedAddress);
-}
-
-async function getPatents() {
-  const numPatents = await PatentContract.methods.numPatents.call().call();
-  let patents = [];
-  for (var i = 0; i < numPatents; i++) {
-    const patent = await PatentContract.methods.patents(i).call();
-    patents.push(patent);
-  }
-  return patents;
-}
-
-async function newPatent(patent) {
-  PatentContract.methods
-    .newPatent(patent.description)
-    .send({ 
-      from: patent.sender,
-      gas: 6721975
-    })
-}
+patents.initializeContract()
 
 app.post('/patent', (req, res) => {
     const patent = req.body;
     console.log(patent);
-    newPatent(patent).then(() => {
+    patents.newPatent(patent).then(() => {
         res.status(200).send('New patent added to blockchain');
     })
     .catch(err => {
@@ -52,7 +20,7 @@ app.post('/patent', (req, res) => {
 });
 
 app.get('/patents', function(req, res) {
-    getPatents()
+    patents.getPatents()
       .then(patents => {
         res.status(200).send(patents);
       })
@@ -60,10 +28,6 @@ app.get('/patents', function(req, res) {
         res.status(400).send('Failed to get patents.');
       });
   });
-
-app.get("/api", (req, res) => {
-  res.json({ message: "Hello from server!" });
-});
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
---
+++ patentrail-be/server/patents.js
@@ -0,0 +1,36 @@
+const PatentStore = require("../build/contracts/Patents.json");
+var Web3 = require("web3");
+
+const web3 = new Web3('ws://localhost:7545');
+var PatentContract;
+
+module.exports.initializeContract = async function () { 
+  const networkId = await web3.eth.net.getId()
+  const deployedAddress = PatentStore.networks[networkId].address
+  console.log(deployedAddress);
+  PatentContract = new web3.eth.Contract(PatentStore.abi, deployedAddress);
+}
+
+module.exports.getPatents = async function () {
+  const numPatents = await PatentContract.methods.numPatents.call().call();
+  let patents = [];
+  for (var i = 0; i < numPatents; i++) {
+    const patent = await PatentContract.methods.patents(i).call();
+    patents.push(patent);
+  }
+  return patents;
+}
+
+module.exports.newPatent = async function (patent) {
+  console.log(PatentContract);
+
+    PatentContract.methods
+    .newPatent(patent.description)
+    .send({ 
+      from: patent.sender,
+      gas: 6721975
+    })
+    .on('receipt', function(){
+        console.log('transaction sent')
+    });
+}
